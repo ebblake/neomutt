de49a653c275729423a4af599269de6f6163fd61
Jakub Jindra <jakub.jindra@socialbakers.com>
2020-10-04 20:05:23 +0200

    deprecate commands pgp-hook, virtual-mailboxes and unvirtual-mailboxes

--- command_parse.c
+++ command_parse.c
@@ -536,6 +536,14 @@ bail:
 enum CommandResult parse_mailboxes(struct Buffer *buf, struct Buffer *s,
                                    intptr_t data, struct Buffer *err)
 {
+  enum CommandResult rc = MUTT_CMD_SUCCESS;
+
+  if (data & MUTT_COMMAND_DEPRECATED)
+  {
+    mutt_warning(_("Command '%s' is deprecated"), buf->data);
+    rc = MUTT_CMD_WARNING;
+  }
+
   while (MoreArgs(s))
   {
     struct Mailbox *m = mailbox_new();
@@ -620,7 +628,7 @@ enum CommandResult parse_mailboxes(struct Buffer *buf, struct Buffer *s,
     mutt_monitor_add(m);
 #endif
   }
-  return MUTT_CMD_SUCCESS;
+  return rc;
 }
 
 /**
@@ -1505,6 +1513,14 @@ static void do_unmailboxes_star(void)
 enum CommandResult parse_unmailboxes(struct Buffer *buf, struct Buffer *s,
                                      intptr_t data, struct Buffer *err)
 {
+  enum CommandResult rc = MUTT_CMD_SUCCESS;
+
+  if (data & MUTT_COMMAND_DEPRECATED)
+  {
+    mutt_warning(_("Command '%s' is deprecated"), buf->data);
+    rc = MUTT_CMD_WARNING;
+  }
+
   while (MoreArgs(s))
   {
     mutt_extract_token(buf, s, MUTT_TOKEN_NO_FLAGS);
@@ -1512,7 +1528,7 @@ enum CommandResult parse_unmailboxes(struct Buffer *buf, struct Buffer *s,
     if (mutt_str_equal(buf->data, "*"))
     {
       do_unmailboxes_star();
-      return MUTT_CMD_SUCCESS;
+      return rc;
     }
 
     mutt_buffer_expand_path(buf);
@@ -1528,7 +1544,7 @@ enum CommandResult parse_unmailboxes(struct Buffer *buf, struct Buffer *s,
       }
     }
   }
-  return MUTT_CMD_SUCCESS;
+  return rc;
 }
 
 /**
